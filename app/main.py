841445cebede41f7c44e5c8ab09d6abd435efa11
Lakshmi Narasimha <lakshminarasimha33333@gmail.com>
2026-06-30 15:44:19 +0530

    cors error

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
 from app.api.chat import router as chat_router
 from app.api.documents import router as document_router
+from fastapi.middleware.cors import CORSMiddleware
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client_url = os.getenv("CLIENT_URL")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[client_url],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     chat_router,
